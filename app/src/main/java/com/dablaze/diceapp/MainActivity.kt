6d12975e87af452cb64f55ea0d4c2b59984c6b87
Dablazer-ufc <chukwuwauchenna@gmail.com>
2019-11-27 13:37:02 +0100

    A simple dice app using kotlin

--- app/src/main/java/com/dablaze/diceapp/MainActivity.kt
+++ app/src/main/java/com/dablaze/diceapp/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         val diceImage: ImageView = findViewById(R.id.imageView_roll)
         rollResult.text = randomNum.toString()
         diceImage.setImageResource(randomPic)
+
     }
 }
 
